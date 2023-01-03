ba8e59a05b43eda31e882cd5ba009cf003a74d90
Mac <mac@MacBook-Pro-de-Mac.local>
2023-01-03 05:39:11 -0800

    commit3

--- estimationapi/views.py
+++ estimationapi/views.py
@@ -458,7 +458,7 @@ def autresImpots(request):
     row = sheet.max_row
     donnes = []
     for i in range(3,row + 1):
-            donnes.append({"nom_province":sheet.cell(i,1).value,"nature_autre_impot":sheet.cell(i,2).value,"autre_impot_urbain":sheet.cell(i,3).value,"autre_impot_rural":sheet.cell(i,4).value});          
+            donnes.append({"nom_province":sheet.cell(i,1).value,"nature_autre_impot":sheet.cell(i,2).value,"autre_impot_urbain":sheet.cell(i,3).value,"autre_impot_urbain_total":sheet.cell(i,4).value,"autre_impot_rural":sheet.cell(i,5).value,"autre_impot_rural_total":sheet.cell(i,6).value});          
     return Response(donnes)
 
 @api_view(['POST'])   
